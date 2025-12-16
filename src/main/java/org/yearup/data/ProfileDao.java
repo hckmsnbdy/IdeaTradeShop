6a4e1b4fecc01800172d4cb7851e7bf622d45453
Omer Faruk Akcam <hckmsnbdy@gmail.com>
2025-12-16 15:32:48 -0500

    Add:    - getByUserId, void update, and method bodies to ProfileDao    - getByUserId,Profile mapRow, and method bodies update to MySqlProfileDao.

--- src/main/java/org/yearup/data/ProfileDao.java
+++ src/main/java/org/yearup/data/ProfileDao.java
@@ -1,9 +1,10 @@
 package org.yearup.data;
 
-
 import org.yearup.models.Profile;
 
 public interface ProfileDao
 {
+    Profile getByUserId(int userId);
     Profile create(Profile profile);
+    void update(Profile profile);
 }
--- src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
+++ src/main/java/org/yearup/data/mysql/MySqlProfileDao.java
@@ -16,6 +16,29 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
     }
 
     @Override
+    public Profile getByUserId(int userId) {
+
+        String sql = "SELECT * FROM profiles WHERE user_id = ?";
+        try (Connection connection = getConnection();)
+        {
+            PreparedStatement statement = connection.prepareStatement(sql);
+            statement.setInt(1, userId);
+
+            ResultSet row = statement.executeQuery();
+
+            if (row.next())
+            {
+                return mapRow(row);
+            }
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+
+    @Override
     public Profile create(Profile profile)
     {
         String sql = "INSERT INTO profiles (user_id, first_name, last_name, phone, email, address, city, state, zip) " +
@@ -42,6 +65,72 @@ public class MySqlProfileDao extends MySqlDaoBase implements ProfileDao
         {
             throw new RuntimeException(e);
         }
+    }
+
+    @Override
+    public void update(Profile profile)
+    {
+        String sql = "UPDATE profile" +
+                " SET user_id = ? " +
+                "   , first_name = ? " +
+                "   , last_name = ? " +
+                "   , phone = ? " +
+                "   , email = ? " +
+                "   , address = ? " +
+                "   , city = ? " +
+                "   , state = ? " +
+                "   , zip = ? " +
+                " WHERE user_id = ?;";
+
+        try (Connection connection = getConnection())
+        {
+            PreparedStatement statement = connection.prepareStatement(sql);
+
+            statement.setInt(1, profile.getUserId());
+            statement.setString(1, profile.getFirstName());
+            statement.setString(4, profile.getLastName());
+            statement.setString(1, profile.getPhone());
+            statement.setString(1, profile.getEmail());
+            statement.setString(1, profile.getAddress());
+            statement.setString(1, profile.getCity());
+            statement.setString(1, profile.getState());
+            statement.setString(1, profile.getZip());
+
+            statement.executeUpdate();
+        }
+        catch (SQLException e)
+        {
+            throw new RuntimeException(e);
+        }
+        // update category
+    }
+
+    private Profile mapRow(ResultSet row) throws SQLException
+    {
+        int userId = row.getInt("user_id");
+        String firstName = row.getString("first_name");
+        String lastName = row.getString("last_name");
+        String phone = row.getString("phone");
+        String email = row.getString("email");
+        String address = row.getString("address");
+        String city = row.getString("city");
+        String state = row.getString("state");
+        String zip = row.getString("zip");
+
+        Profile profile = new Profile()
+        {{
+            setUserId(userId);
+            setFirstName(firstName);
+            setLastName(lastName);
+            setPhone(phone);
+            setEmail(email);
+            setAddress(address);
+            setCity(city);
+            setState(state);
+            setZip(zip);
+        }};
+
+        return profile;
     }
 
 }
